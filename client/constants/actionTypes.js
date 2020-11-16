fdf6c6395a972df374b178c72260b13d8aef7069
Lara <lenichols602@gmail.com>
2020-11-16 16:21:22 -0600

    (add) added an actionTypes file
    Co-authored-by: elisebare elisebare@gmail.com

---
+++ client/constants/actionTypes.js
@@ -0,0 +1,16 @@
+/**
+ * ************************************
+ *
+ * @module  actionTypes.js
+ * @author
+ * @date
+ * @description all the action types
+ *
+ * ************************************
+ */
+
+//subscribe, unsubscribe, message, addClient
+export const SUBSCRIBE = 'SUBSCRIBE';
+export const UNSUBSCRIBE = 'UNSUBSCRIBE';
+export const MESSAGE = 'MESSAGE';
+export const ADD_CLIENT = 'ADD_CLIENT';
