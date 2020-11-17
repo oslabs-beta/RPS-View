e757a112c64c177fa7deca29511302d1c95c8115
Elise Bare <elisebare@gmail.com>
2020-11-17 10:36:36 -0800

    (fix) merged provider changes

--- client/index.js
+++ client/index.js
@@ -2,27 +2,17 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-<<<<<<< HEAD
-import { Provider } from 'react-redux';
-import App from './containers/App.jsx';
-import store from './store.js';
-=======
 import { Provider } from "react-redux";
 import App from './containers/App.jsx';
 import store from "./store";
 
->>>>>>> 8b14b9d2723fd8981cf70db04f7a457c18236bdb
 
 // uncomment so that webpack can bundle styles
 // import styles from './scss/application.scss';
 
 render(
-<<<<<<< HEAD
-  <Provider store = { store }>
-=======
 
   <Provider store={store}>
->>>>>>> 8b14b9d2723fd8981cf70db04f7a457c18236bdb
     <App />
   </Provider>,
   document.getElementById('root')
