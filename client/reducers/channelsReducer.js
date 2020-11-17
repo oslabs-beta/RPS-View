05370f91a1b16656a8f44983ba79663ec1ea8edf
Mark Washkewicz <mwashkewicz@gmail.com>
2020-11-16 18:45:11 -0800

    added add and delete of channels

--- client/reducers/channelsReducer.js
+++ client/reducers/channelsReducer.js
@@ -18,14 +18,51 @@ const initialState = {
 
 const channelsReducer = (state = initialState, action) => {
   let channelList;
-  
+
     switch(action.type){
 
         //add channel 
-        //delete channel
-        //update channel subscribers
-        //update channel messages
+        case types.ADD_CHANNEL:
+            //create a new channel object
+            let newChannel = {
+                //connect with ws and redis? 
+                name : action.payload,
+                messages: []
+            }
 
+            channelList = state.channelList.slice();
+            channelList.push(newChannel);
+
+            return{
+                totalChannels : totalChannels + 1,
+                channelList
+            }
+        
+        
+        
+        //delete channel
+        case types.DELETE_CHANNEL:
+            //create new array
+            //iterate through old array
+            //add elements that arent the delete name on to new array
+            //decrease total channels
+            //update state
+            let updatedArr = [];
+            let currentList = state.channelList;
+            currentList.forEach(ele => {
+              if(ele.name !== action.payload) {
+                updateArr.push(ele)
+              }
+            })
+            return {
+                totalChannels : totalChannels -1,
+                channelList : updatedArr
+            }
+        
+        //add channel subscribers
+
+        //delete channel subscribers
+        //update channel messages
         default:
             return state;
     }
