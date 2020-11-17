198e3d52bdb27a3714dcbd3aa62437ebc3cfb30d
Mark Washkewicz <mwashkewicz@gmail.com>
2020-11-16 18:45:27 -0800

    added channel container and components folder

---
+++ client/containers/ChannelContainer.jsx
@@ -0,0 +1,10 @@
+/**
+ * ************************************
+ *
+ * @module  ChannelContainer
+ * @authors joeseph & mark
+ * @date 
+ * @description stateful component that reders 
+ *
+ * ************************************
+ */
