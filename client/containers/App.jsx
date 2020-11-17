dd4d1acd5959a42827fb5b3755e32945ff767ad4
Joseph <etoh101@gmail.com>
2020-11-17 15:24:54 -0800

    (add) added channel reducers, channelInput change on click

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -13,12 +13,14 @@ import React, {Component} from 'react';
 import '../styles/styles.scss';
 import ClientMenu from './ClientMenu.jsx';
 import ChannelContainer from './ChannelContainer.jsx';
+import NavBar from './NavBar.jsx';
 
 class App extends Component {
   render(){
     return (
       <>
         <h2>hello the app is loading</h2>
+        <NavBar/>
         <ClientMenu />
         <ChannelContainer />
       </>
