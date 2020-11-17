61a99d16beffd45a2a61837c9e55eb0db80eccbd
Lara <lenichols602@gmail.com>
2020-11-16 22:06:05 -0600

    added Types

--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -14,3 +14,5 @@ export const SUBSCRIBE = 'SUBSCRIBE';
 export const UNSUBSCRIBE = 'UNSUBSCRIBE';
 export const MESSAGE = 'MESSAGE';
 export const ADD_CLIENT = 'ADD_CLIENT';
+export const HANDLE_CLIENT_INPUT = 'HANDLE_CLIENT_INPUT';
+export const SET_CLIENT = 'SET_CLIENT';
