ba8ea7b4fa839bd41489ea96d48a8746a968db53
Elise Bare <elisebare@gmail.com>
2020-11-17 09:01:18 -0800

    (update) imported createStore and made store with devTools
    Co-authored-by: Lara lenichols602@gmail.com

--- client/store.js
+++ client/store.js
@@ -8,3 +8,14 @@
  *
  * ************************************
  */
+
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from './reducers/index.js';
+
+
+const store = createStore(
+  reducers, composeWithDevTools()
+);
+
+export default store;
