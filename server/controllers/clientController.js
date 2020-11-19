de2bd5e4438d3020338b78b6da0d27a26333febd
Mark Washkewicz <mwashkewicz@gmail.com>
2020-11-18 19:59:56 -0800

    (adds) added routers and controllers for client and menu
    Co-authored-by: Joe etoh101@gmail.com

---
+++ server/controllers/clientController.js
@@ -0,0 +1,17 @@
+let clientController = {};
+
+
+clientController.addClient = (req, res, next) => {
+  next()
+};
+clientController.unsubscribe = (req, res, next) => {
+  next()
+};
+clientController.subscribe = (req, res, next) => {
+  next()
+};
+clientController.publish = (req, res, next) => {
+  next()
+};
+
+module.exports = clientController;
