c4bfcf79b02eff799ab449eaae69b5f6cd4d67d5
Lara <lenichols602@gmail.com>
2020-11-19 18:59:09 -0600

    added fetchAddChannel

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -151,3 +151,29 @@ export const fetchConnect = (port) => (dispatch) => {
 }
 
 
+//fetchAddChannel
+export const fetchAddChannel = (channelName) => (dispatch) => {
+  fetch('/menu/addChannel', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({channelName}),
+  })
+  .then(response => {
+    if (response.status === 200) {
+      console.log("channel added");
+      dispatch(addChannel(channelName));
+    } 
+    if (response.status >= 400 && response.status < 500) {
+      console.log("Bad URL");
+    } 
+    else if (response.status >= 500) {
+      console.log("server error");
+    }
+  })
+  .catch((error) => {
+    console.log("Error: ", error);
+  })
+}
+
