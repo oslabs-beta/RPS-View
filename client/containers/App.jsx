b08aa3cfd172e85e02600b8bb8b9387454d4b024
Lara <lenichols602@gmail.com>
2020-11-23 21:27:38 -0600

    (update) add tags for styling

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -15,12 +15,9 @@ import ClientMenu from './ClientMenu.jsx';
 import ChannelContainer from './ChannelContainer.jsx';
 import ClientWindow from './ClientWindow.jsx';
 import NavBar from './NavBar.jsx';
-<<<<<<< HEAD
 import img from '../../static/RPS_View_logo.png';
 // import ErrorBox from './ErrorBox.jsx'
-=======
 import ErrorBox from './ErrorBox.jsx'
->>>>>>> a0c7ecc27ad4f415e4578bae00efd0b148f6e1d3
 
 class App extends Component {
   
