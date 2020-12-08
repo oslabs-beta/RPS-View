f0b4284fd66a0828c96a143ef4e847bd1770bb19
Elise Bare <elisebare@gmail.com>
2020-12-08 15:39:50 -0800

    (update) added define plugin to webpack to attempt to change URL based on node env by passing node env onto main
    Co-authored-by: Lara lenichols602@gmail.com

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { BrowserWindow, app } = require('electron')
+require('dotenv').config();
 // const express = require('./server/server.js')
 
 let mainWindow = null
 
 function main() {
+  
   // express
   mainWindow = new BrowserWindow({ width: 1200, height: 800 })
   mainWindow.loadURL(`http://localhost:3000/`)
